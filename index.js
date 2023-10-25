48b9ff809366358e1c6a3836b1946130d73ccadc
Kingston Spotts <guitarhero0122@gmail.com>
2023-10-25 11:47:14 -0400

    almost done

---
+++ index.js
@@ -0,0 +1,55 @@
+//random quote api
+const quoteAPIUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+const quoteSection = document.getElementById('quote');
+const userInput = document.getElementById('quote-input');
+
+let quote = "";
+let time = 60;
+let timer = "";
+let mistakes = 0;
+
+//display tandom quotes
+const renderNewQuote = async () => {
+    const response = await fetch(quoteAPIUrl);
+    let data = await response.json();
+    quote = data.content;
+
+    let arr = quote.split("").map((value) => {
+        return "<span class='quote-chars'>" + value + "</span>";
+    });
+    quoteSection.innerHTML = arr.join("");
+};
+
+userInput.addEventListener("input", () => {
+    let quoteChars = document.querySelectorAll(".quote-chars");
+    quoteChars = Array.from(quoteChars);
+
+    let userInputChars = userInput.value.split('');
+    quoteChars.forEach((char, index) => {
+        if (char.innerText == userInputChars[index]) {
+            char.classList.add("success");
+        }
+
+        else if (userInputChars[index] == null) {
+        if (char.classList.contains("success")) {
+            char.classList.remove("success");
+        } else {
+            char.classList.remove("fails");
+        }
+        }
+        else {
+            if(!char.classList.contains("fail")) {
+                mistakes++;
+                char.classList.add("fail");
+            }
+            document.getElementById("mistakes").innerText = mistakes;
+        }
+        let check = quoteChars.every((element) => {
+            return element.classList.contains("success");
+        });
+
+        if (check) {
+            displayResult();
+        }
+    });
+});
